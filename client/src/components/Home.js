18fbcdea75230d292750a6349be2f45f5ca4ae3b
Chloe <chloepeng331@icloud.com>
2022-04-30 22:50:02 -0400

    search

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,24 +1,32 @@
-// import React from 'react'
+import React from 'react'
+import Search from './Search'
+import Listing from './Listing'
+import HouseMarketReport from './HouseMarketReport'
 
-// function Home() {
-//   return (
-//     <div>Home</div>
-//   )
-// }
+function Home() {
+  return (
+    <div>
+        <h1>Home</h1>
+        <Search />
+        <Listing />
+        <HouseMarketReport />
+    </div>
+  )
+}
 
-// export default Home
+export default Home
 
-import React from "react";
+// import React from "react";
 // import NavBar from "./NavBar";
 // import Grid from '@mui/material/Grid';
-function Home({itemsToDisplay, onCategoryChange, selectedCategory, onGenderChange, selectedGender, setSelectedCauses, causes}) {
+// function Home({itemsToDisplay, onCategoryChange, selectedCategory, onGenderChange, selectedGender, setSelectedCauses, causes}) {
 
 
-    return (
-        <div>
-            <h1>Home</h1>
-        </div>
-    )
-};
+//     return (
+//         <div>
+//             <h1>Home</h1>
+//         </div>
+//     )
+// };
 
-export default Home;
+// export default Home;
